fe57df590662003f9ff637bed07d3b867c169a62
unknown <connectwithadarsha@gmail.com>
2021-02-24 08:59:09 +0545

    add webapp one

---
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,76 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // IActonResult
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // ActionResut
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // Content Result
+        public ContentResult Content()
+        {
+            return Content("<h1>Hello, I am from Content Result</h1>");
+        }
+
+        // Json result
+        public JsonResult MyJson()
+        {
+            return Json(new { name = "Aadarsha", age = 20, address = "Purano Naikap" });
+        }
+
+        // redirect result
+        public RedirectResult MyProfile()
+        {
+            return Redirect(url: "http://www.twitter.com/aadarshatweets");
+        }
+
+        // partial view
+        public PartialViewResult MyPartialView()
+        {
+            return PartialView("_PartialView");
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentViewModel stu)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Message = stu.Fullname + " " + stu.Email; // Message can be named anything
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/StudentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class StudentViewModel
+    {
+        [Required]
+        public string Fullname { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+       
+        [Required]
+        public string State { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+
+    }
+}
